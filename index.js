1c172307dc2d0d80eb34f936a6ec70c5b56e4437
theromulanz <theromulanz@yahoo.com>
2015-04-19 13:05:13 -0700

    Demo

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 var express = require('express');
 var app = express();
 var cool = require('cool-ascii-faces');
+var fs = require('fs');
+var bodyParser = require('body-parser');
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
 
 app.get('/', function(request, response) {
-  var result = ' ';
-  var times = process.env.TIMES || 5;
-  for (i = 0; i < times; i++)
-    result += cool();
-  response.send(result);
+    var obj = JSON.parse(fs.readFileSync('master.json', 'utf8'));
+    var result = ' ';
+    var times = process.env.TIMES || 5;
+    for (i = 0; i < times; i++)
+        result += cool();
+    response.send(obj);
+});
+
+// POST method route
+app.post('/', function (request, response) {
+    var festJson = JSON.parse(fs.readFileSync('master.json', 'utf8'));
+    response.send(festJson);
 });
 
 app.listen(app.get('port'), function() {
