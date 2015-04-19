9310d3cb788081f3f8c09f3bd984160dcb9b2447
theromulanz <theromulanz@yahoo.com>
2015-04-19 14:40:41 -0700

    stuff

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 
-app.use(function(req, res, next) {
+/*app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-
+*/
 app.get('/', function(request, response) {
     var obj = JSON.parse(fs.readFileSync('master.json', 'utf8'));
     var result = ' ';
@@ -26,10 +26,15 @@ app.get('/', function(request, response) {
 // POST method route
 app.post('/', function (request, response) {
     var fest = JSON.parse(fs.readFileSync('master.json', 'utf8'));
-
+    var componentName = request["body"]["componentName"];
+    var action = request["body"]["action"];
+    var resource = request["body"]["resource"];
+    var resource2 = request["body"]["resource2"];
     response.send(request["body"]["componentName"]);
 });
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
 });
+
+
