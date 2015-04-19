7706469625d7a38c5a06b69b3e301f771507f4b2
theromulanz <theromulanz@yahoo.com>
2015-04-19 14:01:21 -0700

    adding cors

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', function(request, response) {
     var obj = JSON.parse(fs.readFileSync('master.json', 'utf8'));
     var result = ' ';
