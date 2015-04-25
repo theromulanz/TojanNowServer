8d1d9a7d9f023c0d32527b423bb568305493a932
theromulanz <theromulanz@yahoo.com>
2015-04-25 10:20:19 -0700

    stuff

--- index.js
+++ index.js
@@ -40,22 +40,23 @@ app.post('/', function (request, response) { // Return only private msg of user!
         if(username in profiles){
             if(profiles[username].password == password){
                 fest["profile"] = profiles[username];
+                user = username;
                 console.log(fest);
                 // set user private thoughts
                 var pvtThoughts4Usr = fest.thoughtJar.filter(function (element) {
                         return (element.name.toLowerCase() == String(user).toLowerCase() || element.name2.toLowerCase() == String(user).toLowerCase());
                 });
                 fest.thoughtJar = pvtThoughts4Usr;
-                
+                console.log("login success: " + user + "---------------------------------");
                 response.send(fest);
             }
             else{
-                console.log("login failure");
+                console.log("login failure ---------------------------------");
                 response.send({"failure": "true"});
             }
         }
         else{
-            console.log("login failure");
+            console.log("login failure ----------------------------------");
             response.send({"failure": "true"});
         }
         
@@ -63,11 +64,10 @@ app.post('/', function (request, response) { // Return only private msg of user!
     else{
         console.log("Already Logged In " + user + "-----------------------------------------------");
         
-        
+        // set user private thoughts
         var pvtThoughts4Usr = fest.thoughtJar.filter(function (element) {
                 return (element.name.toLowerCase() == String(user).toLowerCase() || element.name2.toLowerCase() == String(user).toLowerCase());
         });
-        
         fest.thoughtJar = pvtThoughts4Usr;
         
         if(componentName == "thoughtCollector" && action == "append"){
